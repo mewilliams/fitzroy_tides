331ee83c3b97db769204c1ee3358d87732c09b96
Megan Williams <megams@noc.ac.uk>
2024-10-30 13:35:25 -0300

    adcpg

--- external_data/adcp_shoa_fitzroy/read_adcp_shoa_fitzroy.m
+++ external_data/adcp_shoa_fitzroy/read_adcp_shoa_fitzroy.m
@@ -9,7 +9,7 @@ for i = 1:61
     tline = fgetl(fid);
 end
 
-data = textscan(fid,repmat('%f',1,52),'Delimiter',',');
+data = textscan(fid,repmat('%f',1,53),'Delimiter',',');
 fclose(fid)
 
 % 2016,4,11,12,20,13.7
@@ -20,7 +20,11 @@ NM_m = data{7};
 
 t = datenum(data{1},data{2},data{3},data{4},data{5},data{6});
 
-for ix = 8:29
+% from file: 
+z = 2.1:1:24.1;
+bin_prof_nmm = [23.80	22.80	21.80	20.80	19.80	18.80	17.80	16.80	15.80	14.80	13.80	12.80	11.80	10.80	9.80	8.80	7.80	6.80	5.80	4.80	3.80	2.80	1.80];
+
+for ix = 8:30
     if ix == 8
         velmag = data{ix};
     elseif ix>8
@@ -28,7 +32,7 @@ for ix = 8:29
     end
 end
 
-for ix = 31:52
+for ix = 31:53
     if ix == 31
         veldir = data{ix};
     elseif ix>8
@@ -41,3 +45,56 @@ figure
 plot(t,data{8})
 
 
+
+u = velmag.*cosd(veldir);
+v = velmag.*sind(veldir);
+
+u = u./1000;
+v = v./1000;
+
+
+figure
+subplot(2,1,1)
+pcolor(t,z,u'), shading flat
+caxis([-1.5 1.5])
+cbax = colorbar;
+ylabel(cbax, 'u = |U|cos(\theta)')
+ylabel('z [m]')
+title('ADCP, Fitzroy SHOA 2016')
+
+subplot(2,1,2)
+pcolor(t,z,v'), shading flat
+caxis([-1.5 1.5])
+
+cbax = colorbar;
+ylabel(cbax,'v = |U|sin(\theta)')
+ylabel('z [m]')
+% datetick2('x')
+colormap(cbrewer('div','RdYlBu',256/2))
+datetick2('x','dd mmm')
+subplot(2,1,2)
+xlabel('2016')
+journal_figure_size_bs(40,10)
+
+
+figure
+plot(u(:,5),v(:,5),'.')
+
+
+figure
+plot(t,mean(u,2,'omitnan'))
+hold all
+plot(t,mean(v,2,'omitnan'))
+datetick2('x')
+legend('<u> = |U|cos(\theta)','<v> = |U|sin(\theta)')
+ylabel('Velocity [m/s]')
+title('Depth-averaged Velocities, Fitzroy')
+
+
+
+figure
+plot(t,mean(u,2,'omitnan'))
+ylabel('Velocity [m/s]')
+
+yyaxis right
+plot(t,NM_m), ylabel('Depth [m]')
